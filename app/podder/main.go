867f4cbac84ea21abca3b6141136e2f29272d1b9
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2020-07-21 01:35:31 +0200

    sneeze at Podders

--- app/podder/main.go
+++ app/podder/main.go
@@ -5,15 +5,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/skwiwel/pod-contagion/health"
 )
 
+var (
+	httpAddr   = flag.String("http", "0.0.0.0:80", "HTTP service address.")
+	healthAddr = flag.String("health", "0.0.0.0:81", "Health service address.")
+)
+
+// stopServerChan will close as a sign to close http servers
+var stopServerChan = make(chan struct{})
+
 func main() {
-	var (
-		httpAddr   = flag.String("http", "0.0.0.0:80", "HTTP service address.")
-		healthAddr = flag.String("health", "0.0.0.0:81", "Health service address.")
-	)
 	flag.Parse()
 
 	log.Println("Starting server...")
@@ -22,28 +27,34 @@ func main() {
 
 	errChan := make(chan error, 10)
 
-	healthServer := http.NewServeMux()
-	healthServer.HandleFunc("/liveness", health.LivenessHandler)
-	healthServer.HandleFunc("/readiness", health.ReadinessHandler)
+	healthMux := http.NewServeMux()
+	healthMux.HandleFunc("/liveness", health.LivenessHandler)
+	healthMux.HandleFunc("/readiness", health.ReadinessHandler)
 	//http.HandleFunc("/health/status", HealthzStatusHandler)
 	//http.HandleFunc("/readiness/status", ReadinessStatusHandler)
+	healthServer := &http.Server{Addr: *healthAddr, Handler: healthMux}
 
 	go func() {
-		errChan <- http.ListenAndServe(*healthAddr, healthServer)
+		errChan <- healthServer.ListenAndServe()
 	}()
 
-	httpServer := http.NewServeMux()
-	httpServer.HandleFunc("/face", faceHandler)
+	httpMux := http.NewServeMux()
+	httpMux.HandleFunc("/face", faceHandler)
+	httpServer := &http.Server{Addr: *httpAddr, Handler: httpMux}
 
 	go func() {
-		errChan <- http.ListenAndServe(*httpAddr, httpServer)
+		errChan <- httpServer.ListenAndServe()
 	}()
 
 	for {
 		select {
+		case <-stopServerChan:
+			if err := httpServer.Close(); err != nil {
+				log.Printf("error on closing: %v\n", err)
+			}
 		case err := <-errChan:
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
 			}
 		}
 	}
@@ -64,6 +75,12 @@ func faceHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "eww\n")
 			health.SetLivenessStatus(http.StatusTeapot)
 			log.Println("sniff")
+			// close the http server
+			close(stopServerChan)
+			// sneeze on some Podders
+			for i := 0; i < 10; i++ {
+				go sneeze()
+			}
 		case "":
 			fmt.Fprintf(w, "Do something!\n")
 		default:
@@ -73,4 +90,21 @@ func faceHandler(w http.ResponseWriter, r *http.Request) {
 	default:
 		fmt.Fprintf(w, "Stop bothering me, please.")
 	}
+}
+
+// sneeze sneezes on a Podder.
+// Since this executable has no way of knowing other Podders exist
+// it will sneeze on the address of it's face.
+// Kubernetes' load balancing service should ensure that whatever
+// the number of Podders, they will get sneezed on an equal amount.
+func sneeze() {
+	formData := url.Values{
+		"action": {"achoo"},
+	}
+	resp, err := http.PostForm(fmt.Sprintf("http://%s/face", *httpAddr), formData)
+	if err != nil {
+		log.Printf("could not sneeze: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
 }
