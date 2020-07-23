da38451019547ed9a17543db8098b2aa4cafa4dd
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2020-07-23 20:11:21 +0200

    fixup! added new args to .yamls

--- app/generic/main.go
+++ app/generic/main.go
@@ -10,10 +10,10 @@ import (
 var (
 	httpAddr    = flag.String("http", "0.0.0.0:80", "HTTP service address.")
 	healthAddr  = flag.String("health", "0.0.0.0:81", "Health service address.")
-	healthDelay = flag.Int("healthDelay", 500, `The time delay between getting infected 
-		and signaling negative health status to Kubernetes probes.`)
-	sneezeInterval = flag.Int("sneezeInterval", 500, `The time interval between sneezes. 
-		The amount of sneezes will depend on how fast the container is killed.`)
+	healthDelay = flag.Int("healthDelay", 500, "The time delay between getting infected "+
+		"and signaling negative health status to Kubernetes probes.")
+	sneezeInterval = flag.Int("sneezeInterval", 500, "The time interval between sneezes. "+
+		"The amount of sneezes will depend on how fast the container is killed.")
 )
 
 func main() {
